b56beb0a849aec87412506e693bb0b0fcb8856c3
Valters Strods <valterstrods@aospa.co>
2016-02-15 22:35:29 +0200

    Update extraction script
    This follows up changes in the vendor/lge/bullhead repository.
    Nothing more to report on.
    
    Change-Id: I3f7e7304a1296c1028f4329ae6e99f831a1fd9ee

--- extract.sh
+++ extract.sh
@@ -36,13 +36,13 @@ echo "  VENDOR_MAKEFILE=$VENDOR_MAKEFILE"
 
 # All hail the common header
 
-HEADER="# Copyright (C) $(date +"%Y") ParanoidAndroid Project
+HEADER="# Copyright $(date +"%Y") ParanoidAndroid Project
 #
 # Licensed under the Apache License, Version 2.0 (the \"License\");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an \"AS IS\" BASIS,
@@ -149,7 +149,7 @@ for FILE in $(cat $BLOBS_TXT | grep -v -E '^ *(#|$)' | sed 's/^[-\/]*//' | sort 
     if [ "$SOURCE" = "adb" ]; then
         adb pull -p -a $FILE $BLOBS_ROOT/$FILE
     else
-        cp $SOURCE/$FILE $BLOBS_ROOT/$FILE
+        cp --no-dereference --preserve=links $SOURCE/$FILE $BLOBS_ROOT/$FILE
     fi
 done
 
